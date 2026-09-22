e2c654d1f42a7c8a4b6a54551a3e9769aecaf713
CooperHampson <132439269+CooperHampson@users.noreply.github.com>
2026-09-22 22:22:36 +0900

    Add smooth scroll to top on route changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import './HeroSection.css';
 
 export function HeroSection() {
   return (
-    <section id="/" className="hero-section" data-header-theme="light">
+    <section className="hero-section" data-header-theme="light">
       <div className="hero-container">
         <img className="hero-img" src={`${import.meta.env.BASE_URL}Images/Hero/xoxxlyheader.png`} />
       </div>
